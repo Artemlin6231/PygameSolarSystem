9487a93c4b3d4e99d9a19c21ae0d4722da781c9f
Shamighoul <71729439+Shamighoul@users.noreply.github.com>
2020-11-11 17:55:50 +0300

    Update solar_input.py

--- solar_project/solar_input.py
+++ solar_project/solar_input.py
@@ -115,6 +115,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             # FIXME!
+            out_file.write(obj.type + ' ' + obj.R + ' ' + obj.color + ' ' + obj.m + ' ' + obj.x + ' ' + obj.y + ' ' + obj.Vx + ' ' + obj.Vy)
 
 
 if __name__ == "__main__":
