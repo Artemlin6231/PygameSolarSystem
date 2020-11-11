c2fca5d017af85ca60ee1c27a7f12e05b6248140
Artemlin6231 <71922548+Artemlin6231@users.noreply.github.com>
2020-11-11 16:34:08 +0300

    Update solar_vis.py

--- solar_project/solar_vis.py
+++ solar_project/solar_vis.py
@@ -20,16 +20,14 @@ window_height = 800
 
 scale_factor = 1
 """Масштабирование экранных координат по отношению к физическим.
-
 Тип: float
-
 Мера: количество пикселей на один метр."""
 
 
 def calculate_scale_factor(max_distance):
     """Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
     global scale_factor
-    scale_factor = 0.5*min(window_height, window_width)/max_distance
+    scale_factor = 0.5 * min(window_height, window_width) / max_distance
     print('Scale factor:', scale_factor)
 
 
@@ -38,13 +36,11 @@ def scale_x(x):
     Принимает вещественное число, возвращает целое число.
     В случае выхода **x** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
-
     Параметры:
-
     **x** — x-координата модели.
     """
 
-    return int(x*scale_factor) + window_width//2
+    return int(x * scale_factor) + window_width // 2
 
 
 def scale_y(y):
@@ -53,15 +49,11 @@ def scale_y(y):
     В случае выхода **y** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
     Направление оси развёрнуто, чтобы у модели ось **y** смотрела вверх.
-
     Параметры:
-
     **y** — y-координата модели.
     """
 
-    return int(-y*scale_factor) + window_height//2
-
-
+    return int(y * scale_factor) + window_height // 2
 if __name__ == "__main__":
     print("This module is not for direct call!")
 
@@ -70,12 +62,11 @@ class Drawer:
     def __init__(self, screen):
         self.screen = screen
 
-
     def update(self, figures, ui):
         self.screen.fill((0, 0, 0))
         for figure in figures:
             figure.drawOn(self.screen)
-        
+
         ui.blit()
         ui.update()
         pg.display.update()
